c5418d70ad6bda1aa39412c1f3b61864950306f0
Shunyue Yuan <43017330+yuanshunyue@users.noreply.github.com>
2020-10-02 14:17:41 -0500

    Add files via upload

--- Audio Python/analysis.py
+++ Audio Python/analysis.py
@@ -3,32 +3,30 @@ import numpy as np
 from scipy.io import wavfile as wave
 from scipy.signal import argrelextrema as arg
 from scipy.signal import find_peaks
-
 #reads in the sample rate and data from the wave file as a buffer array
 sampleRate, audio = wave.read("audio06.wav")
 
-#range of data you want to look at in multiples of the sampleRate
-#you can set this to whatever you want
-rateRange = int(sampleRate / 10)
-#the centered time you want to look at
-#for t = 1, you go from 1 - rateRange to 1 + rateRange
-time = 1
-#distance to centered time
-dist = sampleRate * time
-#convert from audio buffer to cleaner numpy data array
-#data = np.frombuffer(audio[dist - rateRange : dist + rateRange], dtype=np.int16)
-data = np.frombuffer(audio, dtype=np.int16)[100:-100]
-#calculate the peaks along the audio signal
-#peaks = arg(data, np.greater)[0]
-peaks = find_peaks(data, height=np.mean(data)+8*np.std(data), distance=5000)[0]
+data = np.frombuffer(audio[:], dtype=np.int16)[100:-100]
+#data = np.frombuffer(audio[:sampleRate*10], dtype=np.int16)[100:-100]
+
+#filter the peaks along the audio signal
+peaks = find_peaks(data, height=np.mean(data)+6*np.std(data), distance=5000)[0]
 print("the first peak is at", peaks[0]/sampleRate, "seconds") # the first peak location
-y = [data[i] for i in peaks] #not quite sure what this does
+peaks_3 = find_peaks(data, height=np.mean(data)+40*np.std(data), distance=15000)[0]
+print("the first peak_3 is at", peaks_3[0]/sampleRate, "seconds")
 
 #plots the audio signal
 plt.plot(data)
-#plots the peaks
-plt.plot(peaks, y, 'ro')
+#plots the peaks of signal_1
+y = [data[i] for i in peaks]
+plt.plot(peaks, y, 'ro',color='r')
+
+#plots the peaks of signal_3
+y_3 = [data[i] for i in peaks_3] 
+plt.plot(peaks_3,y_3,'ro',color='g')
+
 plt.ylabel("Amplitude")
 plt.xlabel("Samples")
 plt.savefig('0.png')
 plt.show()
+
